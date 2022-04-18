e3a125bf8e3e8973bc4da7f41c3d8caac5f82e96
KernPhilip <Philip_Kern@outlook.de>
2022-04-18 18:46:09 +0200

    Added Client and Server project to test multiplayer

---
+++ MultiplayerTestProjects/MultiplayerClientTest/Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,79 @@
+using RiptideNetworking;
+using RiptideNetworking.Utils;
+using System;
+using UnityEngine;
+
+public enum ClientToServerId : ushort
+{
+    name = 1,
+}
+public class NetworkManager : MonoBehaviour
+{
+    private static NetworkManager _sigleton;
+    public static NetworkManager Singleton
+    {
+        get => _sigleton;
+        private set
+        {
+            if (_sigleton == null)
+            {
+                _sigleton = value;
+            }
+            if (_sigleton != value)
+            {
+                Debug.Log($"{nameof(NetworkManager)} instance already exists, destroying duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+    
+    public Client Client { get; private set; }
+    
+    [SerializeField] private string ip;
+    [SerializeField] private ushort port;
+
+    private void Awake()
+    {
+        Singleton = this;
+    }
+
+    private void Start()
+    {
+        RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
+
+        Client = new Client();
+        Client.Connected += DidConnect;
+        Client.ConnectionFailed += FailedToConnect;
+        Client.Disconnected += DidDisconnect;
+    }
+
+    private void FixedUpdate()
+    {
+        Client.Tick();
+    }
+
+    private void OnApplicationQuit()
+    {
+        Client.Disconnect();
+    }
+
+    public void Connect()
+    {
+        Client.Connect($"{ip}:{port}");
+    }
+
+    private void DidConnect(object sender, EventArgs e)
+    {
+        UIManager.Singleton.SendName();
+    }
+
+    private void FailedToConnect(object sender, EventArgs e)
+    {
+        UIManager.Singleton.BackToMain();
+    }
+
+    private void DidDisconnect(object sender, EventArgs e)
+    {
+        UIManager.Singleton.BackToMain();
+    }
+}
