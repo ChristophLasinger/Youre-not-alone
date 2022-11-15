51fe34f7a2bac3e6b36f6d08b9e6f01e2379e0e6
KernPhilip <Philip_Kern@outlook.de>
2022-11-15 01:13:06 +0100

    close #17

--- UnityProjects/YouAreNotAlone_Multiplayer/Assets/Scripts/MenuScripts/LobbyMenu.cs
+++ UnityProjects/YouAreNotAlone_Multiplayer/Assets/Scripts/MenuScripts/LobbyMenu.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using UnityEngine.UI;
+using MLAPI.Transports.UNET;
+using System;
 
 public class LobbyMenu : MonoBehaviour
 {
     public GameObject menuPanel;
     public GameObject backgroundImage;
+    private string ipAddress = string.Empty;
+    private string port = string.Empty;
+    public InputField ipInputField;
+    public InputField portInputField;
 
     public void Host()
     {
@@ -17,8 +24,24 @@ public class LobbyMenu : MonoBehaviour
 
     public void Join()
     {
-        NetworkManager.Singleton.StartClient();
-        menuPanel.SetActive(false);
-        backgroundImage.SetActive(false);
+        if(ipAddress != string.Empty && port != string.Empty)
+        {
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = ipAddress;
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectPort = Int32.Parse(port);
+            NetworkManager.Singleton.StartClient();
+            menuPanel.SetActive(false);
+            backgroundImage.SetActive(false);
+        }
+    }
+    public void ReadUserIpAddress()
+    {
+        ipAddress = ipInputField.text;
+        Debug.Log(ipAddress);
+    }
+    public void ReadUserPort()
+    {
+        port = portInputField.text;
+        Debug.Log(port);
+        
     }
 }
